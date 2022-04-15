9e740ec7fb72c4d3d67c64d1ea16db21096bb56b
RM-Ulster <101607358+RM-Ulster@users.noreply.github.com>
2022-04-16 00:56:40 +0100

    Added code file

---
+++ Projct Code.py
@@ -0,0 +1,12 @@
+# Step 1: Classify data
+
+
+# Step 2: Calculate % green space of each electoral ward
+
+
+# Step 3: Calculate change from previous instance
+
+
+# Step 4: Output map showing GS changes
+
+
