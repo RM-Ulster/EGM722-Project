7278ca61d1688618d91fcfdcc450d7489ace62fc
RM-Ulster <101607358+RM-Ulster@users.noreply.github.com>
2022-05-08 11:21:41 +0100

    Both pieces of code working

--- Project Code Part 1.py
+++ Project Code Part 1.py
@@ -17,31 +17,31 @@ import matplotlib.patches as mpatches
 
 # 2 Load and prepare files for analysis
 
-# 2.1 Load the datasets from the data_files folder
+# i Load the datasets from the data_files folder
 wards = gpd.read_file('data_files/Electoral_Wards.shp')
 green_space = gpd.read_file('data_files/OS_Green_Space.shp')
 
-# 2.2 Ensure CRS is the same for both files
+# ii Ensure CRS is the same for both files
 wards = wards.to_crs(epsg=27700)
 green_space = green_space.to_crs(epsg=27700)
 
-# 2.2.1 Verify that CRS is the same for both
+# iii Verify that CRS is the same for both
 print('CRS matches: ', (green_space.crs == wards.crs))
 
-# 2.3 Clip Green Space shapefile to London area
+# iv Clip Green Space shapefile to London area
 green_clip = gpd.clip(green_space, wards)
 
 # 3 Calculations
 
-# 3.1 Calculate area of each ward
+# i Calculate area of each ward
 
 for i, row in wards.iterrows():
     wards.loc[i, 'area_calc'] = row['geometry'].area
 
-# 3.1.1 Show/hide table in output
+# ii Show/hide table in output
 print(wards.head(10))
 
-# 3.2 Calculate area of green space in each ward
+# iii Calculate area of green space in each ward
 ward_clip = wards[wards['NAME'] == 'ward']
 
 for i, row in wards.iterrows():
@@ -50,24 +50,24 @@ for i, row in wards.iterrows():
     wards.loc[i, 'GS_Area'] = tmp_clip['Area'].sum()
     wards.loc[i, 'ward_name'] = row['NAME']
 
-# 3.2.1 Show/hide table in output
+# iv Show/hide table in output
 print(wards.head(10))
 
-# 3.3 Sum area of green space in each ward
+# v Sum area of green space in each ward
 gs_sum = (wards.groupby(['NAME'])['GS_Area'].sum())
 print(gs_sum)
 
-# 3.4 Join gs_sum to wards table
+# vi Join gs_sum to wards table
 joined = wards.set_index('NAME').join(gs_sum.rename('Total GS Area'))
 
-# 3.5 Calculate percentage of green space in each ward
+# vii Calculate percentage of green space in each ward
 for i, row in joined.iterrows():
     joined.loc[i, 'gs_percent'] = ((row['Total GS Area'] / row['area_calc']) * 100)
 
-# 3.5.1 Show/hide table in output
+# viii Show/hide table in output
 print(joined.head(10))
 
-# 3.6 save shapefile with calculated columns
+# ix save shapefile with calculated columns
 # joined.to_file("Output/joined.shp")
 
 # 4 Output green space percentage to a map
@@ -82,30 +82,30 @@ def generate_handles(labels, colors, edge='k', alpha=0.5):
 
 plt.ion()
 
-# Set projection
+# i Set projection
 myCRS = ccrs.UTM(29)
 
-# Prepare map area
+# ii Prepare map area
 fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=myCRS))
 
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
 
-# Add ward outlines
+# iii Add ward outlines
 ward_outlines = ShapelyFeature(joined['geometry'], myCRS, edgecolor='darkgray', facecolor='none', linewidth=0.25)
 
 ax.add_feature(ward_outlines)
 county_handles = generate_handles([''], ['none'], edge='darkgray')
 
-# Data bounds and colour scale
+# iv Data bounds and colour scale
 gs_plot = joined.plot(column='gs_percent', ax=ax, vmin=0, vmax=50, cmap='Greens',
                        legend=True, cax=cax, legend_kwds={'label': 'Green Space Percentage (%)'})
 
 
-# Legend
+# v Legend
 ax.legend(county_handles, ['Ward Boundaries'], fontsize=12, loc='lower right', framealpha=1)
 
-# Copyright info
+# vi Copyright info
 ax.text(0, 0, 'Contains National Statistics data © Crown copyright and database right (2015) \n '
                            'Contains Ordnance Survey data © Crown copyright and database right (2015)',
         verticalalignment='bottom',
@@ -113,9 +113,9 @@ ax.text(0, 0, 'Contains National Statistics data © Crown copyright and database
         transform=ax.transAxes,
         fontsize=6)
 
-# Title
+# vii Title
 ax.set_title('Publicly Accessible Green Space as a Proportion of Ward Area in Greater London',
              fontweight="bold")
 
-# 4.1 save the map
+# viii save the map
 fig.savefig('Output/OS_Green_Space_Percent_b.png', dpi=300, bbox_inches='tight')
--- Project Code Part 2.py
+++ Project Code Part 2.py
@@ -19,35 +19,35 @@ import matplotlib.patches as mpatches
 
 # 2 Load and prepare files for analysis
 
-# 2.1 Load the datasets from the data_files folder
+# i Load the datasets from the data_files folder
 wards = gpd.read_file('data_files/Electoral_Wards.shp')
 gs_2011 = gpd.read_file('data_files/2011_gs.shp')
 gs_2018 = gpd.read_file('data_files/2018_gs.shp')
 
-# 2.2 Ensure CRS is the same for all files
+# ii Ensure CRS is the same for all files
 wards = wards.to_crs(epsg=27700)
 gs_2011 = gs_2011.to_crs(epsg=27700)
 gs_2018 = gs_2018.to_crs(epsg=27700)
 
-# 2.2.1 Verify that CRS is the same for all files
+# iii Verify that CRS is the same for all files
 print('2011 CRS matches wards: ', (gs_2011.crs == wards.crs))
 print('2018 CRS matches wards: ', (gs_2018.crs == wards.crs))
 
-# 2.3 Clip Green Space shapefiles to London area
+# iv Clip Green Space shapefiles to London area
 gs_2011_clip = gpd.clip(gs_2011, wards)
 gs_2018_clip = gpd.clip(gs_2018, wards)
 
 # 3 Calculations
 
-# 3.1 Calculate area of each ward
+# i Calculate area of each ward
 
 for i, row in wards.iterrows():
     wards.loc[i, 'area_calc'] = row['geometry'].area
 
-# 3.1.1 Show/hide table in output
+# ii Show/hide table in output
 #print(wards.head(10))
 
-# 3.2 Calculate area of green space in each ward for both 2011 and 2018
+# iii Calculate area of green space in each ward for both 2011 and 2018
 
 for i, row in wards.iterrows():
     tmp_clip = gpd.clip(gs_2011_clip, row['geometry'])
@@ -58,22 +58,22 @@ for i, row in wards.iterrows():
     wards.loc[i, '2018_GS_Area'] = tmp_clip['Area'].sum()
     wards.loc[i, 'ward_name'] = row['NAME']
 
-# 3.2.1 Show/hide table in output
+# iv Show/hide table in output
 print(wards.head(0))
 
-# 3.3 Sum area of green space in each ward
+# v Sum area of green space in each ward
 gs_2011_sum = (wards.groupby(['NAME'])['2011_GS_Area'].sum())
 print(gs_2011_sum)
 gs_2018_sum = (wards.groupby(['NAME'])['2018_GS_Area'].sum())
 print(gs_2018_sum)
 
-# 3.4 Join both green space sums to 'wards' table to create a new table named 'joined'
+# vi Join both green space sums to 'wards' table to create a new table named 'joined'
 join_temp = wards.set_index('NAME').join(gs_2011_sum.rename('2011_GS_Sum'))
 for i, row in join_temp.iterrows():
     join_temp.loc[i, 'NAME'] = row['ward_name']
 joined = join_temp.set_index('NAME').join(gs_2018_sum.rename('2018_GS_Sum'))
 
-# 3.5 Calculate percentage of green space in each ward
+# vii Calculate percentage of green space in each ward
 for i, row in joined.iterrows():
     joined.loc[i, 'gs_perc_2011'] = ((row['2011_GS_Sum'] / row['area_calc']) * 100)
     joined.loc[i, 'gs_perc_2018'] = ((row['2018_GS_Sum'] / row['area_calc']) * 100)
@@ -81,10 +81,10 @@ for i, row in joined.iterrows():
 for i, row in joined.iterrows():
     joined.loc[i, 'gs_change'] = (row['gs_perc_2018'] - row['gs_perc_2011'])
 
-# 3.5.1 Show/hide table in output
+# viii Show/hide table in output
 print(joined.head(10))
 
-# 3.6 save shapefile with calculated columns
+# ix save shapefile with calculated columns
 # joined.to_file("Output/gs_2011_2018_diff.shp")
 
 # 4 Output green space percentage to a map
@@ -99,29 +99,29 @@ def generate_handles(labels, colors, edge='k', alpha=0.5):
 
 plt.ion()
 
-# Set projection
+# i Set projection
 myCRS = ccrs.UTM(29)
 
-# Prepare map area
+# ii Prepare map area
 fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=myCRS))
 
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
 
-# Add ward outlines
+# iii Add ward outlines
 ward_outlines = ShapelyFeature(joined['geometry'], myCRS, edgecolor='darkgray', facecolor='none', linewidth=0.25)
 
 ax.add_feature(ward_outlines)
 county_handles = generate_handles([''], ['none'], edge='darkgray')
 
-# Data bounds and colour scale
+# vi Data bounds and colour scale
 gs_plot = joined.plot(column='gs_change', ax=ax, vmin=-25, vmax=25, cmap='RdYlGn',
                        legend=True, cax=cax, legend_kwds={'label': 'Green Space Percentage Change (2011-2018) (%)'})
 
-# Legend
-ax.legend(county_handles, ['Ward Boundaries'], fontsize=12, loc='upper left', framealpha=1)
+# v Legend
+ax.legend(county_handles, ['Ward Boundaries'], fontsize=12, loc='lower right', framealpha=1)
 
-# Copyright info
+# vi Copyright info
 ax.text(0, 0, 'Contains National Statistics data © Crown copyright and database right (2015) \n '
             'Contains Ordnance Survey data © Crown copyright and database right (2015)',
         verticalalignment='bottom',
@@ -129,9 +129,9 @@ ax.text(0, 0, 'Contains National Statistics data © Crown copyright and database
         transform=ax.transAxes,
         fontsize=6)
 
-# Title
+# vii Title
 ax.set_title('Change In Proportion Of Area That Is Green Space (2011-2018)',
              fontweight="bold")
 
-# 4.1 save the map
+# viii save the map
 fig.savefig('Output/Green_Space_Change.png', dpi=300, bbox_inches='tight')
